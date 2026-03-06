0815e1948c47f4146ad2f61d91f3f4bbb0559f4f
Hakan Alsancak <hakanalsancak03@gmail.com>
2026-03-06 22:33:24 +0000

    Initial Commit

---
+++ I Can/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  I Can
+//
+//  Created by Hakan Alsancak on 06/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ I Can/I_CanApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_CanApp.swift
+//  I Can
+//
+//  Created by Hakan Alsancak on 06/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct I_CanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
